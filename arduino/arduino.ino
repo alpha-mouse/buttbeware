fbd2419638474cc6a98aef2806a128f89369aa36
Ivan Bakhtin <ivan.bachtin@gmail.com>
2018-03-20 15:50:05 +0300

    tandem working

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,33 +1,16 @@
-#include <LowPower.h>
 #include <SD.h>
 #include <TMRpcm.h>
-#include <SPI.h>
 
 #define SDChipSelectPin 4
 #define MusicPCMPin 9
-#define RCLedPin 2
-#define RelayPin 5
 #define LightPin 6
-
-#define TriggerDuration 300
-#define TriggerFrequency 480
+#define BatteryVoltageDividerPin 5
 #define LightPhaseDuration 800
 
-enum state_t {
-  S_Initializing = 0,
-  S_WaitingTrigger = 1,
-  S_TriggerCandidate = 2,
-  S_Triggered = 3,
-  S_Playing = 4,
-};
-
 const char music[] = "mrm.wav";
-const long MusicLength = 34500;
+const long MusicDuration = 34500;
 TMRpcm tmrpcm;
-volatile byte currentState = S_Initializing;
-volatile long lastStateChangeTime;
-volatile int pulsesReceived = 0;
-
+long startTime;
 
 void setup() {
 
@@ -42,91 +25,42 @@ void setup() {
     message("ok SD");
   }
 
-  pinMode(RelayPin, OUTPUT);
   pinMode(LightPin, OUTPUT);
-  pinMode(RCLedPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(RCLedPin), rcSignal, RISING);
+  pinMode(BatteryVoltageDividerPin, OUTPUT);
+  digitalWrite(BatteryVoltageDividerPin, HIGH);
+
+  tmrpcm.play(music);
+  startTime = millis();
 
   message("Startup ok");
-  goToState(S_WaitingTrigger);
-  sleep();
 }
 
 void loop() {
-  long duration;
-
-  switch (currentState) {
-    case S_WaitingTrigger:
-      sleep();
-      break;
-    case S_TriggerCandidate:
-      duration = millis() - lastStateChangeTime;
-      if (duration < TriggerDuration) {
-        break;
-      } else {
-        if (pulsesReceived < (long)TriggerFrequency * duration / 1000 / 2) {
-          // false positive
-          message(pulsesReceived);
-          goToState(S_WaitingTrigger);
-          sleep();
-        } else {
-          message(pulsesReceived);
-          goToState(S_Triggered);
-        }
-        break;
-      }
-    case S_Triggered:
-      digitalWrite(RelayPin, HIGH);
-      tmrpcm.play(music);
-      goToState(S_Playing);
-      break;
-    case S_Playing:
-      duration = millis() - lastStateChangeTime;
-      if (MusicLength < duration) {
-        digitalWrite(RelayPin, LOW);
-        digitalWrite(LightPin, LOW);
-        digitalWrite(MusicPCMPin, LOW);
-        goToState(S_WaitingTrigger);
-        sleep();
-        break;
-      } else {
-        digitalWrite(LightPin, duration / LightPhaseDuration % 2 == 0 ? HIGH : LOW);
-        break;
-      }
-  }
-}
-
-void sleep() {
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-}
-
-void goToState(state_t state) {
-  message("to state ", state);
-  currentState = state;
-  lastStateChangeTime = millis();
-}
-
-void rcSignal() {
-  if (currentState == S_WaitingTrigger) {
-    goToState(S_TriggerCandidate);
-    pulsesReceived = 1;
-  } else if (currentState == S_TriggerCandidate) {
-    pulsesReceived++;
+  long duration = millis() - startTime;
+  if (MusicDuration < duration) {
+    digitalWrite(LightPin, LOW);
+    digitalWrite(MusicPCMPin, LOW);
+    while (true) ; // wait for death
+  } else {
+    digitalWrite(LightPin, duration / LightPhaseDuration % 2 == 0 ? HIGH : LOW);
   }
 }
 
 void message(char str[]) {
+  return;
   Serial.println(str);
   Serial.flush();
 }
 
 void message(char str[], int i) {
+  return;
   Serial.print(str);
   Serial.println(i);
   Serial.flush();
 }
 
 void message(long i) {
+  return;
   Serial.println(i);
   Serial.flush();
 }
--- attiny/attiny.ino
+++ attiny/attiny.ino
@@ -1,9 +1,12 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
+
 #include <avr/sleep.h>
 
 #define TriggerDuration 300
 #define TriggerFrequency 480
+#define AlarmingOwnBatteryLevel 3600
+#define AlarmingOtherBatteryLevel 8000
+#define OtherBatteryVoltageDivision 6
 
 enum state_t {
   S_Initializing = 0,
@@ -18,7 +21,11 @@ volatile byte currentState = S_Initializing;
 volatile long lastStateChangeTime;
 volatile int pulsesReceived = 0;
 
-// interrupt
+boolean ownBatteryLow = false;
+boolean otherBatteryLow = false;
+volatile boolean batteryLowWarningLit = false;
+
+// RC interrupt
 ISR(INT0_vect)
 {
   if (currentState == S_WaitingTrigger) {
@@ -29,29 +36,72 @@ ISR(INT0_vect)
   }
 }
 
+int watchdogIteration = 0; // so that warning lights are lit once a minute approx.
+// watchdog low battry level signaling
+ISR(WDT_vect)
+{
+  if (++watchdogIteration == 8) {
+    watchdogIteration = 0;
+    if (ownBatteryLow) {
+      PORTB |= _BV(PB4);
+    }
+    if (otherBatteryLow) {
+      PORTB |= _BV(PB1);
+    }
+    batteryLowWarningLit = true;
+  }
+}
+
 void setup() {
 
-  DDRB |= _BV(DDB0); // pin 5 output
-  DDRB &= ~_BV(DDB2); // pin 7 input
+  DDRB |= _BV(DDB0); // pin 5 output swith downstream
+  DDRB &= ~_BV(DDB3); // pin 2 input downstream voltage
+  DDRB |= _BV(DDB4); // pin 3 output own voltage warning
+  DDRB |= _BV(DDB1); // pin 6 output downstream voltage warning
 
-  // interrupt setup
-  GIMSK |= _BV(INT0);
-  MCUCR &= ~_BV(ISC00);
-  MCUCR &= ~_BV(ISC01);
+  DDRB &= ~_BV(DDB2); // pin 7 input RC interrupt
+
+  // RC interrupt setup
+  GIMSK |= _BV(INT0); // enable interrupt
+  MCUCR |= _BV(ISC01); // falling edge
+
+  // check batteries on startup
+  checkBatteries();
+  if (!(ownBatteryLow || otherBatteryLow)) {
+    // flash short if all fine
+    PORTB |= _BV(PB4) || _BV(PB1);
+    delay(5);
+    PORTB &= ~(_BV(PB4) || _BV(PB1));
+  } else {
+    // flash for 2sec if some battery is low
+    if (ownBatteryLow) {
+      PORTB |= _BV(PB4);
+    }
+    if (otherBatteryLow) {
+      PORTB |= _BV(PB1);
+    }
+    delay(2000);
+    PORTB &= ~(_BV(PB4) || _BV(PB1));
+    startLowBatteryWatchdog();
+  }
 
   ADCSRA &= ~_BV(ADEN); // switch ADC off so it doesn't use current
-
   goToState(S_WaitingTrigger);
   sleep();
-  
+
 }
 
 void loop() {
   long duration;
 
+  if (batteryLowWarningLit) {
+    delay(1);
+    PORTB &= ~(_BV(PB4) || _BV(PB1));
+    batteryLowWarningLit = false;
+  }
+
   switch (currentState) {
     case S_WaitingTrigger:
-      // shouldn't happen actually, but still
       sleep();
       break;
     case S_TriggerCandidate:
@@ -75,6 +125,10 @@ void loop() {
     case S_Playing:
       duration = millis() - lastStateChangeTime;
       if (TurnOnDuration < duration) {
+        checkBatteries();
+        if (ownBatteryLow || otherBatteryLow) {
+          startLowBatteryWatchdog();
+        }
         PORTB &= ~_BV(PB0);
         goToState(S_WaitingTrigger);
         sleep();
@@ -94,8 +148,51 @@ void sleep() {
   sei();
 }
 
+void startLowBatteryWatchdog() {
+  WDTCR |= _BV(WDP3) | _BV(WDP0);// watchdog prescaler 1k ~8sec
+  WDTCR |= _BV(WDIE); // enable watchdog
+}
+
 void goToState(state_t state) {
   currentState = state;
   lastStateChangeTime = millis();
+}
+
+void checkBatteries() {
+  ADCSRA |= _BV(ADEN); // turn ADC on
+
+  int ownVoltage = readOwnVcc();
+  int otherVoltage = readOtherVoltage(ownVoltage);
+
+  ownBatteryLow = ownVoltage < AlarmingOwnBatteryLevel;
+  otherBatteryLow = otherVoltage < AlarmingOtherBatteryLevel;
+
+  ADCSRA &= ~_BV(ADEN); // switch ADC off again
+}
+
+
+int readOwnVcc() {
+
+  // From http://digistump.com/wiki/digispark/quickref
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+
+  delay(10); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA, ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high << 8) | low;
+
+  result = 1042000L / result; // Calculate Vcc (in mV); 1125300 = 1.017*1023*1000 (1.017 from adjusting oneself)
+
+  return (int)result; // Vcc in millivolts
+}
+
+int readOtherVoltage(int reference) {
+  long voltage = analogRead(3);
+  return reference * voltage * OtherBatteryVoltageDivision / 1023;
 }
 
